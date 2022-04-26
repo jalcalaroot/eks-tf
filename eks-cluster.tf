6aefd9000b068e131a7d42f058214a8ea242a882
jalcalaroot <johanalcala104@gmail.com>
2022-04-26 15:50:11 -0300

    deploy

--- eks-cluster.tf
+++ eks-cluster.tf
@@ -12,10 +12,10 @@ module "eks" {
   source          = "terraform-aws-modules/eks/aws"
   version         = "17.24.0"
   cluster_name    = local.cluster_name
-  cluster_version = "1.22.6"
-  subnets         = module.vpc.private_subnets
+  cluster_version = "1.22.6":
+  subnets         = ["subnet-09db50623a8f31326", "subnet-0a8128188062b2435", "subnet-059d93a0d3918d586"]
 
-  vpc_id = module.vpc.vpc_id
+  vpc_id = "vpc-09bf547bfdc87e81f"
 
   workers_group_defaults = {
     root_volume_type = "gp3"
