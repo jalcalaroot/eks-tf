9ea45997c5e1dae0eac4d916aad34b383c116848
jalcalaroot <johanalcala104@gmail.com>
2022-04-26 15:34:20 -0300

    deploy

---
+++ eks-cluster.tf
@@ -0,0 +1,53 @@
+resource "random_string" "suffix" {
+  length  = 4
+  special = false
+}
+
+module "vpc" {
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "3.2.0
+
+
+module "eks" {
+  source          = "terraform-aws-modules/eks/aws"
+  version         = "17.24.0"
+  cluster_name    = local.cluster_name
+  cluster_version = "1.22.6"
+  subnets         = module.vpc.private_subnets
+
+  vpc_id = module.vpc.vpc_id
+
+  workers_group_defaults = {
+    root_volume_type = "gp3"
+    enable_monitoring = "true"
+    root_volume_size = "100"
+  }
+
+  worker_groups = [
+    {
+      name                          = "worker-group-1"
+      instance_type                 = "t3a.medium"
+      additional_userdata           = "echo foo bar"
+      additional_security_group_ids = [aws_security_group.worker_group_mgmt_one.id]
+      asg_desired_capacity          = 2
+    },
+  ]
+}
+
+#    {
+#      name                          = "worker-group-2"
+#      instance_type                 = "t2.medium"
+#      additional_userdata           = "echo foo bar"
+#      additional_security_group_ids = [aws_security_group.worker_group_mgmt_two.id]
+#      asg_desired_capacity          = 0
+#    },
+#  ]
+#}
+
+data "aws_eks_cluster" "cluster" {
+  name = module.eks.cluster_id
+}
+
+data "aws_eks_cluster_auth" "cluster" {
+  name = module.eks.cluster_id
+}
